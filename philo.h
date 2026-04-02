d0621929a9dd5056cb527dd9c3ca63de9effe61a
Aristidescosta <aristidiscosta200@gmail.com>
2026-04-02 14:56:38 +0100

    feat: adicionado a validação de argumentos

--- philo.h
+++ philo.h
@@ -1,5 +1,6 @@
 #ifndef PHILO_H
 #define PHILO_H
 #include <stdio.h>
+#include <unistd.h>
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,9 +1,36 @@
 #include "../philo.h"
 
+static int	valid_args(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	if (argc < 5 || argc > 6)
+		return (0);
+	i = 1;
+	while (i < argc)
+	{
+		j = 0;
+		while (argv[i][j])
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+				return (0);
+			j++;
+		}
+		if (argv[i][0] == '0')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
 int	main(int argc, char **argv)
 {
-	(void)argc;
-	(void)argv;
+	if (!valid_args(argc, argv))
+	{
+		write(2, "Error: Invalid arguments\n", 25);
+		return (1);
+	}
 	
 	printf("DINING PHILOSOPHERS SOLUCTION\n");
 	return (0);
